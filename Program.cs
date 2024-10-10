978a60379783f8dc0c0fff8b556ab0b9d8d6e811
gabrielmrib7 <gabrielmrib@hotmail.com>
2024-10-10 04:09:37 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.Clear();
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456","Nokia Tijolão", "1111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("777777","Iphone 16", "2222222222", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
